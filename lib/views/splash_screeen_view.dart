3c3ebdd345e16a02595d1d8dc143f650a4a27801
muzaffar <muzsamgio@gmail.com>
2024-07-20 19:09:03 +0500

    splash

--- lib/views/splash_screeen_view.dart
+++ lib/views/splash_screeen_view.dart
@@ -16,15 +16,48 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
 
     // Delayed navigation
-    Future.delayed(const Duration(seconds: 5), () {
-      context.go(login_page); // Ensure you have the correct route name
-    });
+    // Future.delayed(const Duration(seconds: 5), () {
+    //   context.go(login_page); // Ensure you have the correct route name
+    // }
+    // );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(child: Text('Welcome')),
+      body: Padding(
+        padding: const EdgeInsets.all(23),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 180),
+            Text(
+              'Lorby',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w500,
+                color: Color(0xff212121),
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'you personal tutor',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+                color: Color(0xff212121),
+              ),
+            ),
+            SizedBox(height: 65),
+            Image(
+              height: 350,
+              width: double.maxFinite,
+              fit: BoxFit.cover,
+              image: AssetImage('assets/images/splash_image.png'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
